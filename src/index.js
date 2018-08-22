36ecf304bae17fa70e2b1d02d8181524b563975c
yangarnet <yangarnet@gmail.com>
2018-08-22 21:34:40 +1000

    add some configurations for production deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
 import hero from "./images/hero.jpg";
 import logo from "./images/logo.png";
+
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <App hero={hero} logo={logo} />,
     document.getElementById("root")
 );
+registerServiceWorker();
